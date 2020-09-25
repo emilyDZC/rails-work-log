16e1093299388021e3d50a93d7c47db396bcd840
Emily Chaplais <emilychaplais@hotmail.com>
2020-09-25 19:24:06 +0100

    Add image magick to fix paperclip resize

--- app/models/concerns/Post.rb
+++ app/models/concerns/Post.rb
@@ -6,9 +6,16 @@ class Post
   field :title
   field :body
 
-  has_mongoid_attached_file :image
+  has_mongoid_attached_file :image,
+    :styles => {
+      :original => ['1920x1680>', :jpg],
+      :small    => ['100x100#',   :jpg],
+      :medium   => ['250x250',    :jpg],
+      :large    => ['500x500>',   :jpg]
+    }
+
   validates_attachment_content_type :image, content_type: /image/
-  # validates_attachment_presence :image, attachment_presence: true, on: :update
+  
   validates_presence_of :title
   validates_presence_of :body
 end
