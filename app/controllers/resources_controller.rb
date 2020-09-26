7990a49a2814396d5f67b926832bf0fca51c515f
Emily Chaplais <emilychaplais@hotmail.com>
2020-09-26 05:03:25 +0100

    Add resources CRUD

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,5 +1,49 @@
 class ResourcesController < ApplicationController
 
   def index
+    @resources = Resource.all
+  end
+
+  def new
+    @resource = Resource.new
+  end
+
+  def create
+    @resource = Resource.create(resource_params)
+    if @resource.save
+      redirect_to resources_path, notice: 'Resource successfully created.'
+    else
+      flash.now.alert = 'Unable to create resource, please check the details and try again.'
+      render :new
+    end
+  end
+
+  def edit
+    @resource = Resource.find(params[:id])
+  end
+
+  def update
+    @resource = Resource.find(params[:id])
+    if @resource.update(resource_params)
+      redirect_to resources_path, notice: 'Resource successfully updated.'
+    else
+      flash.now.alert = 'Unable to update resource, please check the details and try again.'
+      render :edit
+    end
+  end
+
+  def show
+    @resource = Resource.find(params[:id])
+  end
+
+  def destroy
+    @resource = Resource.find(params[:id])
+    @resource.destroy
+  end
+
+  private
+
+  def resource_params
+    params.require(:resource).permit(:name, :link, :image)
   end
 end
--- app/models/Resource.rb
+++ app/models/Resource.rb
@@ -1,6 +1,18 @@
 class Resource
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::Paperclip
   
-  field :name
+  field :name, type: String
+  field :link, type: String
+
+  has_mongoid_attached_file :image,
+    :styles => {
+      :original => ['1920x1680>', :jpg],
+      :small    => ['100x100#',   :jpg],
+      :medium   => ['250x250',    :jpg],
+      :large    => ['500x500>',   :jpg]
+    }
+
+  validates_attachment_content_type :image, content_type: /image/
 end
